12fcf7e5a9152eb918b905a6c378ab322140ced9
William Rodriguez <william@rodriguez1.com>
2022-04-01 13:52:38 -0700

    Add user inputted name to roboger catchphrase

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,10 @@ function getRange(max) {
   return range;
 }
 
-function beepBoop(max, digits) {
+function beepBoop(max, digits, name) {
   return getRange(max).map(function(number) {
     if(includesDigit(number, digits[2])) {
-      return "Won\'t you be my neighbor?";
+      return "Won\'t you be my neighbor, " + name + "?";
     } else if(includesDigit(number, digits[1])) {
       return "Boop";
     } else if (includesDigit(number, digits[0])) {
@@ -44,9 +44,9 @@ $(document).ready(function() {
     e.preventDefault();
 
     const digits = [parseInt($("#digit-replace-1").val()), parseInt($("#digit-replace-2").val()), parseInt($("#digit-replace-3").val())];
-
+    const name = $("#name").val();
     const number = $("#number").val();
-    const counting = beepBoop(number, digits);
+    const counting = beepBoop(number, digits, name);
 
     const outputList = $("#output ul");
     outputList.html("");
